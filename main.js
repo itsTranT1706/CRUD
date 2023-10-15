27ecbf743b1103be425695ade71eab607e71eeb1
itsTranT1706 <khiemtrangia60@gmail.com>
2023-10-15 15:21:41 +0700

    format code

--- main.js
+++ main.js
@@ -7,57 +7,57 @@ fetch(url1)
         let htmls = json.map((data) => {
             return `<tr> <th scope="row">${data.id}</th><td>${data.name}</td><td>${data.price}</td><td><button>delete</button></td></tr>`
         })
-        let html = htmls.join(``); 
+        let html = htmls.join(``);
         lengthAdi = json.length;
         let table1 = document.querySelector("#table-1");
         table1.innerHTML = html;
-        
+
     })
-    
+
 
     .catch(() => {
         alert("can't get data from  api (adidas)")
-    
+
     })
 
-    let adiElement = document.querySelector("#adi"); 
-    adiElement.onclick = function(e) {
-        const element = document.getElementById("adidas");
-        element.scrollIntoView();
-        element.scrollIntoView(false);
-        element.scrollIntoView({ block: "end" });
-        element.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
-    }
+let adiElement = document.querySelector("#adi");
+adiElement.onclick = function (e) {
+    const element = document.getElementById("adidas");
+    element.scrollIntoView();
+    element.scrollIntoView(false);
+    element.scrollIntoView({ block: "end" });
+    element.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+}
 
-    let nikElement = document.querySelector("#nik"); 
-    nikElement.onclick = function(e) {
-        const element = document.getElementById("nike");
-        element.scrollIntoView();
-        element.scrollIntoView(false);
-        element.scrollIntoView({ block: "end" });
-        element.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
-    }
+let nikElement = document.querySelector("#nik");
+nikElement.onclick = function (e) {
+    const element = document.getElementById("nike");
+    element.scrollIntoView();
+    element.scrollIntoView(false);
+    element.scrollIntoView({ block: "end" });
+    element.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+}
 
 let submitAdi = document.querySelector("#submitAdi");
-submitAdi.onclick = function() {
-    let nameAdi = document.querySelector("#staticEmail2").value; 
-    let priceAdi= document.querySelector("#inputPassword2").value; 
+submitAdi.onclick = function () {
+    let nameAdi = document.querySelector("#staticEmail2").value;
+    let priceAdi = document.querySelector("#inputPassword2").value;
     // let idAdi = lengthAdi++;
     // console.log(idAdi);
-    createProduct({name:nameAdi, id : 7, price: priceAdi });
+    createProduct({ name: nameAdi, id: 7, price: priceAdi });
 }
 function createProduct(product) {
     fetch("http://localhost:3000/adidas", {
         method: "POST",
         body: JSON.stringify({
-           id: product.id,
-           name: product.name,
-           price: product.price
+            id: product.id,
+            name: product.name,
+            price: product.price
         }),
         headers: {
-          "Content-type": "application/json; charset=UTF-8"
+            "Content-type": "application/json; charset=UTF-8"
         }
-      });
+    });
 }
 
 
